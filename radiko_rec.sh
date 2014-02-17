d6445d2bd923bab90f207d269ddd0ddac896f80c
haru <haru@yukikaze.dyndns.org>
2014-02-17 16:52:01 +0900

    整形

--- radiko_rec.sh
+++ radiko_rec.sh
@@ -1,110 +1,123 @@
-#!/bin/ksh
-#-----------------------------------------------------------------
-# $1:ID (必須)
-#       TBS      TBSラジオ
-#       QRR      文化放送
-#       LFR      ニッポン放送
-#       NSB      ラジオNIKKEI
-#       INT      INTERFM
-#       FMT      TOKYO FM
-#       BAYFM78  BayFm
-#       NACK5    NACK5
-#       JORF     ラジオ日本
-#       YFM      FM yokohama
-#       ---------------------
-#       ABC      ABCラジオ
-#       MBS      MBSラジオ
-#       OBC      ラジオ大阪
-#       CCL      FM COCOLO
-#       802      FM802
-#       FMO      FM大阪
-#
-# $2:OFILE (必須)
-#       任意のファイル名(UTF-8可)
-# $3:TIME (必須)
-#       録音時間(分)
-#
-# -t "VALUE" (省略可)
-#       タイトル
-# -a "VALUE" (省略可)
-#       アーティスト
-# -A "VALUE" (省略可)
-#       アルバム
-# -g "VALUE" (省略可)
-#       ジャンル
-#-----------------------------------------------------------------
-export LANG="ja_JP.UTF-8" LC_ALL="ja_JP.UTF-8"
+#!/bin/sh
 
-USAGE=\
-"Usage:$0 ID OFILE TIME [-t \"TITLE\"] [-a \"ARTIST\"] [-A \"ALBUM\"] [-g \"GENRE\"]"
+date=`date '+%Y-%m-%d-%H:%M'`
+playerurl=http://radiko.jp/player/swf/player_3.0.0.01.swf
+playerfile=./player.swf
+keyfile=./authkey.png
 
-if [ $# -lt 3 ];then
-    print "${USAGE}\n`head -n36 $0 | grep -v ksh | sed 's/^#//g' `" 1>&2
-    exit 1
-fi
-
-ID="$1"
-OFILE="$2"
-TIME=$3
-#------------------------------------------#
-GetAuth_SRC="${HOME}/bin/get_auth.sh"
-#------------------------------------------#
-TOUT=1800  # sec
-DELAY=20   # sec
-RDIR=/tmp
-CODEC=libmp3lame
-####CODEC=libfaac
-((TIME=TIME*60+DELAY))
-BITRATE=65536
-
-DATE1=$(date +%Y-%m-%d_%H.%M.%S)
-DATE2=$(echo ${DATE1} | cut -c1-10)
-YEAR=$(echo ${DATE1} | cut -c1-4)
-
-shift 3
-while getopts a:g:t:A: opt
-do
-    case ${opt} in
-        t) TITLE="${OPTARG}";;
-        a) AUTHOR="${OPTARG}";;
-        g) GENRE="${OPTARG}";;
-        A) ALBUM="${OPTARG}";;
-        *) echo ${USAGE} 1>&2
-           exit 1;;
-    esac
-done
-
-if [ "${CODEC}" = "libmp3lame" ];then
-    RFILE="${RDIR}/${ID}_${OFILE}_${DATE1}.mp3"
+if [ $# -eq 2 ]; then
+  station=$1
+  DURATION=`expr $2 \* 60`
 else
-    RFILE="${RDIR}/${ID}_${OFILE}_${DATE1}.m4a"
+  echo "usage : $0 station_name duration(minuites)"
+  exit 1
 fi
 
-### authentication
-RAuth=`${GetAuth_SRC}`
-set -- ${RAuth}
+#
+# get player
+#
+if [ ! -f $playerfile ]; then
+  wget -q -O $playerfile $playerurl
 
-playerurl=$1
-authtoken=$2
+  if [ $? -ne 0 ]; then
+    echo "failed get player"
+    exit 1
+  fi
+fi
 
-/usr/local/bin/rtmpdump -B ${TIME} -m ${TOUT} -qvr \
-    rtmpe://w-radiko.smartstream.ne.jp/${ID}/_definst_/simul-stream.stream \
-    -W ${playerurl} -C S:"" -C S:"" -C S:"" -C S:${authtoken} \
-    -o - 2>/dev/null | \
-/opt/local/bin/ffmpeg \
-    -metadata author="${AUTHOR}" \
-    -metadata artist="${AUTHOR}" \
-    -metadata title="${TITLE}" \
-    -metadata album="${ALBUM}" \
-    -metadata genre="${GENRE}" \
-    -metadata year="${YEAR}" \
-    -y -i - -vn -acodec ${CODEC} -ar 44100 -ab ${BITRATE} -ac 2 "${RFILE}" > /dev/null 2>&1
+#
+# get keydata (need swftool)
+#
+if [ ! -f $keyfile ]; then
+  swfextract -b 14 $playerfile -o $keyfile
 
-#iTunes_DIR=`find "${HOME}" -name "iTunes に自動的に追加" -type d | grep -vi trash`
-#if [ "${iTunes_DIR}" ]; then
-#    cp "${RFILE}" "${iTunes_DIR}"
-#    if [ "$?" = "0" ]; then
-#        rm -f "${RFILE}"
-#    fi
-#fi
+  if [ ! -f $keyfile ]; then
+    echo "failed get keydata"
+    exit 1
+  fi
+fi
+
+if [ -f auth1_fms ]; then
+  rm -f auth1_fms
+fi
+
+#
+# access auth1_fms
+#
+wget -q \
+     --header="pragma: no-cache" \
+     --header="X-Radiko-App: pc_1" \
+     --header="X-Radiko-App-Version: 2.0.1" \
+     --header="X-Radiko-User: test-stream" \
+     --header="X-Radiko-Device: pc" \
+     --post-data='\r\n' \
+     --no-check-certificate \
+     --save-headers \
+     https://radiko.jp/v2/api/auth1_fms
+
+if [ $? -ne 0 ]; then
+  echo "failed auth1 process"
+  exit 1
+fi
+
+#
+# get partial key
+#
+authtoken=`perl -ne 'print $1 if(/x-radiko-authtoken: ([\w-]+)/i)' auth1_fms`
+offset=`perl -ne 'print $1 if(/x-radiko-keyoffset: (\d+)/i)' auth1_fms`
+length=`perl -ne 'print $1 if(/x-radiko-keylength: (\d+)/i)' auth1_fms`
+
+partialkey=`dd if=$keyfile bs=1 skip=${offset} count=${length} 2> /dev/null | base64`
+echo "authtoken: ${authtoken} \noffset: ${offset} length: ${length} \npartialkey: 
+
+$partialkey"
+
+rm -f auth1_fms
+
+if [ -f auth2_fms ]; then
+  rm -f auth2_fms
+fi
+
+#
+# access auth2_fms
+#
+wget -q \
+     --header="pragma: no-cache" \
+     --header="X-Radiko-App: pc_1" \
+     --header="X-Radiko-App-Version: 2.0.1" \
+     --header="X-Radiko-User: test-stream" \
+     --header="X-Radiko-Device: pc" \
+     --header="X-Radiko-Authtoken: ${authtoken}" \
+     --header="X-Radiko-Partialkey: ${partialkey}" \
+     --post-data='\r\n' \
+     --no-check-certificate \
+     https://radiko.jp/v2/api/auth2_fms
+
+if [ $? -ne 0 -o ! -f auth2_fms ]; then
+  echo "failed auth2 process"
+  exit 1
+fi
+
+echo "authentication success"
+areaid=`perl -ne 'print $1 if(/^([^,]+),/i)' auth2_fms`
+echo "areaid: $areaid"
+
+rm -f auth2_fms
+
+#
+# rtmpdump
+#
+rtmpdump -v \
+         -r "rtmpe://w-radiko.smartstream.ne.jp" \
+         --playpath "simul-stream.stream" \
+         --app "${station}/_definst_" \
+         -W $playerurl \
+         -C S:"" -C S:"" -C S:"" -C S:$authtoken \
+         --live \
+         --stop $DURATION \
+         -o "/tmp/${1}_${date}"
+
+ffmpeg -y -i "/tmp/${1}_${date}" -acodec libmp3lame "/tmp/${1}_${date}.mp3"
+
+#rm "/tmp/${1}_${date}"
 
